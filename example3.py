f5b17aaafc3e605141b90008ed12c7ca93fd1484
Danil Smirnov <danil@smirnov.la>
2020-05-29 13:07:37 +0000

    Fixes

--- example3.py
+++ example3.py
@@ -12,16 +12,16 @@ def index():
     print('Error!')
     raise NotFoundError('Nothing in this path!')
 
-@app.route('/user', methods=['POST'])
+@app.route('/users', methods=['POST'])
 def create_user():
     user_as_json = app.current_request.json_body
-    response = dynamodb.update_item(TableName=f'user-{env}',
+    response = dynamodb.update_item(TableName=f'users-{env}',
         Key={'UserName': {'S': user_as_json['name']}},
         UpdateExpression='SET UserData=:d',
         ExpressionAttributeValues={':d': {'S': json.dumps(user_as_json)}})
     return {'user': user_as_json}
 
-@app.route('/user/{name}')
+@app.route('/users/{name}')
 def get_user(name):
-    response = dynamodb.get_item(TableName=f'user-{env}', Key={'UserName': {'S': name}})
+    response = dynamodb.get_item(TableName=f'users-{env}', Key={'UserName': {'S': name}})
     return {'user': response['Item']}
